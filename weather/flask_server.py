ebeeb540c5a679177c056bb90c5b9734e35d1ee7
nikmend <nikmend_@hotmail.com>
2021-02-18 23:59:28 -0500

    First commit, with the skeleton of the project, simple functionality

---
+++ weather/flask_server.py
@@ -0,0 +1,37 @@
+from flask import (
+    Flask, request
+)
+import json
+from weather.openweathermap import get_weather
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'this is my weather API!'
+
+
+@app.route('/weather', methods=['GET'])
+def weather():
+    try:
+        if request.method == 'GET':
+            """
+            city=$City&country=$Country
+            """
+            city = request.args.get('city')
+            country = request.args.get('country')
+            print(city, country)
+            data = get_weather(cityName=city, isoCountry=country)
+            response = app.response_class(
+                response=json.dumps(data),
+                status=200,
+                mimetype='application/json'
+            )
+            return response
+
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+
+
+app.run()
---
+++ weather/utils.py
@@ -0,0 +1,7 @@
+import os
+
+
+# Get environment variables
+
+def get_GEO_API_KEY():
+    return os.getenv('GEO_API_KEY')
